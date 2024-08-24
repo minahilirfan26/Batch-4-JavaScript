9b904f8cedfabc4a14aa9c3a98331305303afbc6
Minahil Irfan <99713184+minahilirfan26@users.noreply.github.com>
2024-08-24 10:42:59 +0500

    Add files via upload

---
+++ class02/app.js
@@ -0,0 +1,68 @@
+/*Question No 1 */
+// alert("Welcome to my website")
+
+/*Question no 2 */
+// alert("error!")
+
+// alert("hello wolrd")
+
+// var message = "Hello world";
+// alert(message)
+// Data types:
+// Number 5656367
+// String ""
+// boolean true false
+
+// var message = "hello world";
+
+// message = "hello coders";
+//  alert(message)
+
+// var num1 = "5";
+// var num2 = 7;
+
+// var totalNum = num1  * num2;
+// alert(totalNum)
+
+// var heading = "<h1>hello world </h1>";
+// document.write(heading)
+
+// var num = 1;
+// alert("You have visited my site " + num + " times")
+
+// num = num + 1;
+// alert("You have visited my site " + num + " times")
+// num = num + 1;
+// alert("You have visited my site " + num + " times")
+// num = num + 1;
+// alert("You have visited my site " + num + " times")
+// num = num + 1;
+// alert("You have visited my site " + num + " times")
+
+//Post
+// var num = 5;
+// alert(num)
+// num--;
+// alert(num)
+// num--;
+// alert(num)
+// num--;
+// alert(num)
+// num--;
+// alert(num)
+// num--;
+// alert(num)
+
+//Pre
+// var num = 2;
+// ++num;
+// alert(num)
+
+var a = 2;
+var b = 4;
+// var tNum = a++ + ++b - a-- * b++;
+  // 2      5    3     5
+// var tNum = (++a - ++b) * b-- + a++ + b + a;
+          // 3   -  5   *   5  +   3  +   4 +  4 
+        //   -32 1 21 -7 
+            // alert(tNum)

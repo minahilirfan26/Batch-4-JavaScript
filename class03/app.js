3c3dc71e35a3cf68472625f40008f7dcfe2a1754
Minahil Irfan <99713184+minahilirfan26@users.noreply.github.com>
2024-08-27 10:51:03 +0500

    Add files via upload

---
+++ class03/app.js
@@ -0,0 +1,24 @@
+// var message1 = "Hello";
+// var userMessage =  message1 + "World";
+// document.write(userMessage)
+
+// var num1 = +prompt("enter the 1st digit");
+// var num2 = +prompt("enter the 2nd digit")
+// console.log(num1 * num2)
+
+
+// var campus = prompt("enter your smit campus")
+// if (campus === "malir"){
+//  console.log("Your teacher is minahil")
+// }
+
+// if (1 !== 2) {
+//     console.log("correct")
+// }
+
+var userAge = +prompt("enter your age");
+if (userAge <= 20) {
+    console.log("Your are allow");
+}else {
+    console.log("Sorry! you are not allow")
+}

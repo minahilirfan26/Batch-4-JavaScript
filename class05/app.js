f65ea167b5f4437ecc65889ae3b66a0f83626825
Minahil Irfan <99713184+minahilirfan26@users.noreply.github.com>
2024-09-14 10:42:14 +0500

    Add files via upload

---
+++ class05/app.js
@@ -0,0 +1,115 @@
+// alert("hello")
+// window.alert("hello")
+
+// var num = 5;
+// num = 10;
+// var num1 = "5";
+// console.log(num , typeof Number(num1))
+//  var num = 5;
+//  num = num + 5;
+//  console.log(num)
+// num++;
+
+// console.log(num)
+// var a = 5;
+// var b = 4;
+// var ab = a++ + b++ + ++a + ++b;
+        //   3     4     5     6
+//   var ab =  ++a - b++ + (a++ * ++b) - b--;
+//             //  6   -  4  +  6  *   6  -    6
+// console.log(ab);
+
+// var a = 10;
+// var b = 15;
+
+// console.log(--a + ++a + b-- * a++ - ++b );
+            // 9      10    15    11    14
+// var name = "Minahil";
+// var fatherName = "Irfan";
+// var fullName = name + " " + fatherName;
+// console.log(fullName)     
+
+// var num1 = +prompt("1st num");
+// var num2 = +prompt("2nd num")
+// console.log(num1 + num2)
+// var message;
+// console.log(message)
+
+// var num = 1;
+// if(num !== "1"){
+//     console.log(true);
+// }
+// else{
+//     console.log(false)
+// }
+//  var age = +prompt("enrter your age");
+//  if(age <= 18){
+//     console.log("Your are allow")
+//  } else{
+//     console.log("Your are not allow")
+//  }
+
+// var trafficLights = prompt("enter the signal");
+// if (trafficLights === "red") {
+//     console.log("must stop!!");
+// } else if (trafficLights === "yellow"){
+//     console.log("ready to go");
+// } else if (trafficLights === "green"){
+//     console.log("move now");
+// };
+
+// var city = prompt("enter your city");
+// console.log(city.toUpperCase())
+// var liveLocation = prompt("enter your live location")
+
+// if (city.toLowerCase() === liveLocation.toLowerCase()) {
+//         console.log("hello world")
+// }else{
+//         console.log("try again")
+// }
+
+// if (city.toLowerCase() === "karachi") {
+//         console.log("Welcome")
+// }
+// var mathsNum = +prompt("enter the num")
+// if (mathsNum === 20) {
+//         console.log("passing num")
+// } else if(mathsNum === 25 ){
+//         console.log("keep shining")
+// } else if (mathsNum === 30){
+//         console.log("welldone")
+// } else if (mathsNum == " "){
+//         alert("enter the num")
+// }else{
+//         console.log("fail")
+// }
+
+// var age = 18;
+// var city = "karachi";
+// var course = "web development";
+//  if (age !== 18 && city === "karachi" && course === "web development") {
+//         console.log("you r enrolled")
+//  } else{
+//         console.log("better luck next time")
+//  }
+
+// if (age === 18 || age === 19 || age === 20) {
+//         console.log("adult")
+// }else{
+//         console.log("you are to old for this position")
+// } 
+
+// 0
+// false
+// NaN
+// null
+// ""
+// undefined
+ 
+// var skill1 = prompt("enter 1st your skills");
+// var skill2 = prompt("enter ypur 2nsd skill")
+// if (skill1 === "HTML") {
+//         if (skill2 === "CSS") {
+//                 console.log("Hired")
+//         } 
+// }
---
+++ class06/app.js
@@ -0,0 +1,22 @@
+// var arr = ["Apple", "Mango", "Orange"];
+// var arr1 = "Apple";
+// var arr2 = "Mango";
+// var arr3 = "Orange"
+// arr[3] = "Grape";
+// console.log(arr[1])
+// arr.push("Grape", "Banana");
+// arr.pop();
+// arr.shift();
+// arr.unshift("Grape", "kivi");
+// var arr = ["Apple", "Mango", "Orange"];
+// arr.splice(1,2,"Grape","kivi")
+// console.log(arr.slice(1,2));
+
+
+//  var arr = [1,2,5];
+// var arr = new Array();
+var arr1 = ["Minahil", "dua", "ayesha", "sana"];
+var arr2 = [400, 450, 300, 350]
+
+document.write(arr1[1] + " your score is" +" " + arr2[1] )
+

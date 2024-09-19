144867c7ef023598ddc0eb91c28620c871b77130
Minahil Irfan <99713184+minahilirfan26@users.noreply.github.com>
2024-09-19 10:48:44 +0500

    Add files via upload

---
+++ class07/app.js
@@ -0,0 +1,58 @@
+
+// for (var i = 0; i < 10; i++) {
+//     console.log(i)
+// }
+
+// var tableNum = +prompt("enter the num");
+// var tableLength = +prompt("enter the table length");
+// console.log(tableLength)
+// for(var i = 1; i <= tableLength; i++){
+//     console.log(tableNum + " " + "x" + "" + i + "" + "=" + tableNum*i );
+// }
+
+
+// var arr = ["apple", "banana", "orange", "grapge"];
+
+// for(i = 0; i <= 20; i = i+2){
+//     // console.log(arr[i]);
+//     // console.log("Element at index " + i + " is " + arr[i])
+//     console.log(i)
+// }
+
+    // var flag = false;
+    // for(i = 1; i <= 10; i++){
+    //     console.log(i);
+    //     flag = false;
+    // };
+
+//   var arr = ["apple", "banana", "orange", "grapge"];
+//   for(i = 0; i < arr.length; i++){
+//     console.log(arr[i])
+//   }
+
+// var myName = "Minahil";
+
+// var splitingVal = myName.split("")
+// console.log(splitingVal);
+// var joingVal = splitingVal.join("")
+// console.log(joingVal);
+
+// for(i = 10; i >= 1; i--){
+//     console.log(i)
+// }
+
+// var bakeryItems = ["cake", "apple pie", "cookie", "chips"];
+// var userInput = prompt("Welcome to my bakery");
+// var itemFound = false; // Flag to track if the item was found
+
+// for (var i = 0; i < bakeryItems.length; i++) {
+//     if (bakeryItems[i] === userInput) {
+//         console.log(`${userInput} is available at index ${i} in our bakery`);
+//         itemFound = true;
+//         break; 
+//     }
+// }
+
+// if (!itemFound) {
+//     console.log(`${userInput} is not available`);
+// }

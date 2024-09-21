c0744f3c588b055c5e90ba1dd8a900b69c35190a
Minahil Irfan <99713184+minahilirfan26@users.noreply.github.com>
2024-09-21 10:23:05 +0500

    Add files via upload

---
+++ class08/app.js
@@ -0,0 +1,40 @@
+// for(var i = 0; i <= 10; i++){
+//  if (i === 5) {
+//     break;
+    
+//  }
+//  console.log(i)
+// }
+
+// let arr1 = ["white","black", "blue"]
+// let arr2 = ["Red", "yellow", "Green"];
+// for(var i = 0; i < arr1.length; i++){
+//     for(var j = 0; j < arr2.length; j++){
+//       console.log(arr1[i] , arr2[j])
+//     }
+// }
+
+// var myName = "minahil irfan";
+// console.log(myName)
+// var words = myName.split(" ");
+// console.log(words)
+// var joiningvAl = words.join(" ")
+// console.log(joiningvAl) 
+
+
+// var myName = "minahil irfan shaikh";
+// var words = myName.split(" ");
+// var arr = [];
+// // console.log(words);
+// for(i = 0; i < words.length; i++){
+//    words[i] = words[i][0].toUpperCase() + words[i].slice(1);
+// //    console.log(words[i])
+// arr.push(words[i]);
+// }
+// // console.log(arr);
+// var fullName = arr.join(" ");
+// console.log(fullName);
+//  var str = "Minahil";
+//  str += "Irfan";
+//  console.log(str);
+
